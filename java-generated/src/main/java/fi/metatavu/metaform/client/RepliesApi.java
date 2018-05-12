b9e43c1c352052cf9d44640505118b0cd383c5ef
Antti Leppä <antti.leppa@metatavu.fi>
2018-05-12 22:11:07 +0300

    Generated source

---
+++ java-generated/src/main/java/fi/metatavu/metaform/client/RepliesApi.java
@@ -0,0 +1,264 @@
+package fi.metatavu.metaform.client;
+
+import fi.metatavu.metaform.ApiClient;
+import fi.metatavu.metaform.EncodingUtils;
+
+import fi.metatavu.metaform.client.BadRequest;
+import fi.metatavu.metaform.client.Forbidden;
+import fi.metatavu.metaform.client.InternalServerError;
+import fi.metatavu.metaform.client.NotFound;
+import fi.metatavu.metaform.client.Reply;
+import fi.metatavu.metaform.client.ReplyMeta;
+import java.util.UUID;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import feign.*;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-05-12T22:10:42.916+03:00")
+public interface RepliesApi extends ApiClient.Api {
+
+
+  /**
+   * create new form reply
+   * Creates new form reply
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param payload Payload (required)
+    * @param updateExisting specifies that existing reply should be updated (optional)
+   * @return Reply
+   */
+  @RequestLine("POST /realms/{realmId}/metaforms/{metaformId}/replies?updateExisting={updateExisting}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  Reply createReply(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, Reply payload, @Param("updateExisting") Boolean updateExisting);
+
+  /**
+   * create new form reply
+   * Creates new form reply
+   * Note, this is equivalent to the other <code>createReply</code> method,
+   * but with the query parameters collected into a single Map parameter. This
+   * is convenient for services with optional query parameters, especially when
+   * used with the {@link CreateReplyQueryParams} class that allows for
+   * building up this map in a fluent style.
+   * @param realmId realm id (required)
+   * @param metaformId Metaform id (required)
+   * @param payload Payload (required)
+   * @param queryParams Map of query parameters as name-value pairs
+   *   <p>The following elements may be specified in the query map:</p>
+   *   <ul>
+   *   <li>updateExisting - specifies that existing reply should be updated (optional)</li>
+   *   </ul>
+   * @return Reply
+   */
+  @RequestLine("POST /realms/{realmId}/metaforms/{metaformId}/replies?updateExisting={updateExisting}")
+  @Headers({
+  "Content-Type: application/json",
+  "Accept: application/json",
+  })
+  Reply createReply(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, Reply payload, @QueryMap(encoded=true) Map<String, Object> queryParams);
+
+  /**
+   * A convenience class for generating query parameters for the
+   * <code>createReply</code> method in a fluent style.
+   */
+  public static class CreateReplyQueryParams extends HashMap<String, Object> {
+    public CreateReplyQueryParams updateExisting(final Boolean value) {
+      put("updateExisting", EncodingUtils.encode(value));
+      return this;
+    }
+  }
+
+  /**
+   * Deletes a reply
+   * Deletes a reply
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param replyId Reply id (required)
+   */
+  @RequestLine("DELETE /realms/{realmId}/metaforms/{metaformId}/replies/{replyId}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  void deleteReply(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("replyId") UUID replyId);
+
+  /**
+   * Exports metaform data
+   * Exports metaform data
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param format Export results in specified format (XLSX) (required)
+   * @return byte[]
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/export?format={format}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  byte[] export(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("format") String format);
+
+  /**
+   * Exports metaform data
+   * Exports metaform data
+   * Note, this is equivalent to the other <code>export</code> method,
+   * but with the query parameters collected into a single Map parameter. This
+   * is convenient for services with optional query parameters, especially when
+   * used with the {@link ExportQueryParams} class that allows for
+   * building up this map in a fluent style.
+   * @param realmId realm id (required)
+   * @param metaformId Metaform id (required)
+   * @param queryParams Map of query parameters as name-value pairs
+   *   <p>The following elements may be specified in the query map:</p>
+   *   <ul>
+   *   <li>format - Export results in specified format (XLSX) (required)</li>
+   *   </ul>
+   * @return byte[]
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/export?format={format}")
+  @Headers({
+  "Content-Type: application/json",
+  "Accept: application/json",
+  })
+  byte[] export(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @QueryMap(encoded=true) Map<String, Object> queryParams);
+
+  /**
+   * A convenience class for generating query parameters for the
+   * <code>export</code> method in a fluent style.
+   */
+  public static class ExportQueryParams extends HashMap<String, Object> {
+    public ExportQueryParams format(final String value) {
+      put("format", EncodingUtils.encode(value));
+      return this;
+    }
+  }
+
+  /**
+   * Find a single reply
+   * Finds single reply by id
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param replyId Reply id (required)
+   * @return Reply
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/replies/{replyId}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  Reply findReply(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("replyId") UUID replyId);
+
+  /**
+   * Returns reply meta
+   * Returns meta data from the reply
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param replyId Reply id (required)
+   * @return ReplyMeta
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/replies/{replyId}/meta")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  ReplyMeta findReplyMeta(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("replyId") UUID replyId);
+
+  /**
+   * Lists form replies
+   * Lists form replies
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param userId Filter results by user id. If this parameter is not specified all replies are returned, this requires logged user to have proper permission to do so (optional)
+    * @param createdBefore Filter results created before specified time (optional)
+    * @param createdAfter Filter results created after specified time (optional)
+    * @param modifiedBefore Filter results modified before specified time (optional)
+    * @param modifiedAfter Filter results modified after specified time (optional)
+    * @param includeRevisions Specifies that revisions should be included into response (optional)
+   * @return List&lt;Reply&gt;
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/replies?userId={userId}&createdBefore={createdBefore}&createdAfter={createdAfter}&modifiedBefore={modifiedBefore}&modifiedAfter={modifiedAfter}&includeRevisions={includeRevisions}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  List<Reply> listReplies(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("userId") UUID userId, @Param("createdBefore") String createdBefore, @Param("createdAfter") String createdAfter, @Param("modifiedBefore") String modifiedBefore, @Param("modifiedAfter") String modifiedAfter, @Param("includeRevisions") Boolean includeRevisions);
+
+  /**
+   * Lists form replies
+   * Lists form replies
+   * Note, this is equivalent to the other <code>listReplies</code> method,
+   * but with the query parameters collected into a single Map parameter. This
+   * is convenient for services with optional query parameters, especially when
+   * used with the {@link ListRepliesQueryParams} class that allows for
+   * building up this map in a fluent style.
+   * @param realmId realm id (required)
+   * @param metaformId Metaform id (required)
+   * @param queryParams Map of query parameters as name-value pairs
+   *   <p>The following elements may be specified in the query map:</p>
+   *   <ul>
+   *   <li>userId - Filter results by user id. If this parameter is not specified all replies are returned, this requires logged user to have proper permission to do so (optional)</li>
+   *   <li>createdBefore - Filter results created before specified time (optional)</li>
+   *   <li>createdAfter - Filter results created after specified time (optional)</li>
+   *   <li>modifiedBefore - Filter results modified before specified time (optional)</li>
+   *   <li>modifiedAfter - Filter results modified after specified time (optional)</li>
+   *   <li>includeRevisions - Specifies that revisions should be included into response (optional)</li>
+   *   </ul>
+   * @return List&lt;Reply&gt;
+   */
+  @RequestLine("GET /realms/{realmId}/metaforms/{metaformId}/replies?userId={userId}&createdBefore={createdBefore}&createdAfter={createdAfter}&modifiedBefore={modifiedBefore}&modifiedAfter={modifiedAfter}&includeRevisions={includeRevisions}")
+  @Headers({
+  "Content-Type: application/json",
+  "Accept: application/json",
+  })
+  List<Reply> listReplies(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @QueryMap(encoded=true) Map<String, Object> queryParams);
+
+  /**
+   * A convenience class for generating query parameters for the
+   * <code>listReplies</code> method in a fluent style.
+   */
+  public static class ListRepliesQueryParams extends HashMap<String, Object> {
+    public ListRepliesQueryParams userId(final UUID value) {
+      put("userId", EncodingUtils.encode(value));
+      return this;
+    }
+    public ListRepliesQueryParams createdBefore(final String value) {
+      put("createdBefore", EncodingUtils.encode(value));
+      return this;
+    }
+    public ListRepliesQueryParams createdAfter(final String value) {
+      put("createdAfter", EncodingUtils.encode(value));
+      return this;
+    }
+    public ListRepliesQueryParams modifiedBefore(final String value) {
+      put("modifiedBefore", EncodingUtils.encode(value));
+      return this;
+    }
+    public ListRepliesQueryParams modifiedAfter(final String value) {
+      put("modifiedAfter", EncodingUtils.encode(value));
+      return this;
+    }
+    public ListRepliesQueryParams includeRevisions(final Boolean value) {
+      put("includeRevisions", EncodingUtils.encode(value));
+      return this;
+    }
+  }
+
+  /**
+   * Updates reply
+   * Updates reply
+    * @param realmId realm id (required)
+    * @param metaformId Metaform id (required)
+    * @param replyId Reply id (required)
+    * @param payload Payload (required)
+   */
+  @RequestLine("PUT /realms/{realmId}/metaforms/{metaformId}/replies/{replyId}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  void updateReply(@Param("realmId") String realmId, @Param("metaformId") UUID metaformId, @Param("replyId") UUID replyId, Reply payload);
+}
