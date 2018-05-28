15869e138dedee248f226622ea68592f25d8343d
Antti Leppä <antti.leppa@metatavu.fi>
2018-05-28 07:24:52 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/metaform/server/rest/RealmsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/metaform/server/rest/RealmsApi.java
@@ -25,7 +25,7 @@ import java.lang.Exception;
 @Api(description = "the realms API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-05-27T15:58:22.920+03:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-05-28T07:24:33.973+03:00")
 
 
 public interface RealmsApi  {
@@ -177,7 +177,7 @@ public interface RealmsApi  {
         @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
         @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
         @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
-    public Response listReplies(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@QueryParam("userId")   @ApiParam("Filter results by user id. If this parameter is not specified all replies are returned, this requires logged user to have proper permission to do so")  UUID userId,@QueryParam("createdBefore")   @ApiParam("Filter results created before specified time")  String createdBefore,@QueryParam("createdAfter")   @ApiParam("Filter results created after specified time")  String createdAfter,@QueryParam("modifiedBefore")   @ApiParam("Filter results modified before specified time")  String modifiedBefore,@QueryParam("modifiedAfter")   @ApiParam("Filter results modified after specified time")  String modifiedAfter,@QueryParam("includeRevisions")   @ApiParam("Specifies that revisions should be included into response")  Boolean includeRevisions) throws Exception;
+    public Response listReplies(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@QueryParam("userId")   @ApiParam("Filter results by user id. If this parameter is not specified all replies are returned, this requires logged user to have proper permission to do so")  UUID userId,@QueryParam("createdBefore")   @ApiParam("Filter results created before specified time")  String createdBefore,@QueryParam("createdAfter")   @ApiParam("Filter results created after specified time")  String createdAfter,@QueryParam("modifiedBefore")   @ApiParam("Filter results modified before specified time")  String modifiedBefore,@QueryParam("modifiedAfter")   @ApiParam("Filter results modified after specified time")  String modifiedAfter,@QueryParam("includeRevisions")   @ApiParam("Specifies that revisions should be included into response")  Boolean includeRevisions,@QueryParam("fields")   @ApiParam("Filter results by field values. Format is field:value, multiple values can be added by using comma separator. E.g. field1&#x3D;value,field2&#x3D;another")  List<String> fields) throws Exception;
 
     @PUT
     @Path("/{realmId}/metaforms/{metaformId}")
