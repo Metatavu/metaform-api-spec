72240c9695e86f0962ecfeb2061ecd8215ee7e3c
Antti Leppä <antti.leppa@metatavu.fi>
2018-08-24 10:28:03 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/metaform/server/rest/RealmsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/metaform/server/rest/RealmsApi.java
@@ -3,6 +3,8 @@ package fi.metatavu.metaform.server.rest;
 import fi.metatavu.metaform.server.rest.model.Attachment;
 import fi.metatavu.metaform.server.rest.model.BadRequest;
 import fi.metatavu.metaform.server.rest.model.EmailNotification;
+import fi.metatavu.metaform.server.rest.model.ExportTheme;
+import fi.metatavu.metaform.server.rest.model.ExportThemeFile;
 import fi.metatavu.metaform.server.rest.model.Forbidden;
 import fi.metatavu.metaform.server.rest.model.InternalServerError;
 import fi.metatavu.metaform.server.rest.model.Metaform;
@@ -26,7 +28,7 @@ import java.lang.Exception;
 @Api(description = "the realms API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-08-23T09:14:44.221+03:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-08-24T10:27:38.539+03:00")
 
 
 public interface RealmsApi  {
@@ -45,6 +47,36 @@ public interface RealmsApi  {
         @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
         @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
     public Response createEmailNotification(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@Valid EmailNotification payload) throws Exception;
+
+    @POST
+    @Path("/{realmId}/exportThemes")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "create new form export theme", notes = "Creates new form export theme", response = ExportTheme.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Created export theme", response = ExportTheme.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response createExportTheme(@PathParam("realmId") @ApiParam("realm id") String realmId,@Valid ExportTheme payload) throws Exception;
+
+    @POST
+    @Path("/{realmId}/exportThemes/{exportThemeId}/files")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "create new export theme file", notes = "Creates new export theme file", response = ExportThemeFile.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes", "ExportThemeFiles",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Created export theme file", response = ExportThemeFile.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response createExportThemeFile(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId,@Valid ExportThemeFile payload) throws Exception;
 
     @POST
     @Path("/{realmId}/metaforms")
@@ -90,6 +122,36 @@ public interface RealmsApi  {
         @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
         @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
     public Response deleteEmailNotification(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@PathParam("emailNotificationId") @ApiParam("Email notification id") UUID emailNotificationId) throws Exception;
+
+    @DELETE
+    @Path("/{realmId}/exportThemes/{exportThemeId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Deletes an export theme", notes = "Deletes an export theme", response = Void.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 204, message = "Empty response indicating a successful removal", response = Void.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response deleteExportTheme(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId) throws Exception;
+
+    @DELETE
+    @Path("/{realmId}/exportThemes/{exportThemeId}/files/{exportThemeFileId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Deletes an export theme file", notes = "Deletes an export theme file", response = Void.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes", "ExportThemeFiles",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 204, message = "Empty response indicating a successful removal", response = Void.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response deleteExportThemeFile(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId,@PathParam("exportThemeFileId") @ApiParam("export theme file id") UUID exportThemeFileId) throws Exception;
 
     @DELETE
     @Path("/{realmId}/metaforms/{metaformId}")
@@ -180,6 +242,36 @@ public interface RealmsApi  {
     public Response findEmailNotification(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@PathParam("emailNotificationId") @ApiParam("EmailNotification id") UUID emailNotificationId) throws Exception;
 
     @GET
+    @Path("/{realmId}/exportThemes/{exportThemeId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Finds single export theme", notes = "Finds single export theme", response = ExportTheme.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "single export theme", response = ExportTheme.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response findExportTheme(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId) throws Exception;
+
+    @GET
+    @Path("/{realmId}/exportThemes/{exportThemeId}/files/{exportThemeFileId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Finds single export theme file", notes = "Finds single export theme file", response = ExportThemeFile.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes", "ExportThemeFiles",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "single export theme file", response = ExportThemeFile.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response findExportThemeFile(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId,@PathParam("exportThemeFileId") @ApiParam("export theme file id") UUID exportThemeFileId) throws Exception;
+
+    @GET
     @Path("/{realmId}/metaforms/{metaformId}")
     @Consumes({ "application/json;charset&#x3D;utf-8" })
     @Produces({ "application/json;charset&#x3D;utf-8" })
@@ -223,6 +315,36 @@ public interface RealmsApi  {
         @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
         @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
     public Response listEmailNotifications(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId) throws Exception;
+
+    @GET
+    @Path("/{realmId}/exportThemes/{exportThemeId}/files")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Lists files of export theme", notes = "Lists files of export theme", response = ExportThemeFile.class, responseContainer = "List", authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes", "ExportThemeFiles",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "A list of export theme files", response = ExportThemeFile.class, responseContainer = "List"),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response listExportThemeFiles(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("export theme id") UUID exportThemeId) throws Exception;
+
+    @GET
+    @Path("/{realmId}/exportThemes")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Lists form export themes", notes = "Lists export themes", response = ExportTheme.class, responseContainer = "List", authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "A list of export themes", response = ExportTheme.class, responseContainer = "List"),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response listExportThemes(@PathParam("realmId") @ApiParam("realm id") String realmId) throws Exception;
 
     @GET
     @Path("/{realmId}/metaforms")
@@ -282,6 +404,36 @@ public interface RealmsApi  {
         @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
         @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
     public Response updateEmailNotification(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("metaformId") @ApiParam("Metaform id") UUID metaformId,@PathParam("emailNotificationId") @ApiParam("EmailNotification id") UUID emailNotificationId,@Valid EmailNotification payload) throws Exception;
+
+    @PUT
+    @Path("/{realmId}/exportThemes/{exportThemeId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Updates export theme", notes = "Updates export theme", response = ExportTheme.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "an updated export theme", response = ExportTheme.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response updateExportTheme(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("ExportTheme id") UUID exportThemeId,@Valid ExportTheme payload) throws Exception;
+
+    @PUT
+    @Path("/{realmId}/exportThemes/{exportThemeId}/files/{exportThemeFileId}")
+    @Consumes({ "application/json;charset&#x3D;utf-8" })
+    @Produces({ "application/json;charset&#x3D;utf-8" })
+    @ApiOperation(value = "Updates export theme file", notes = "Updates export theme file", response = ExportThemeFile.class, authorizations = {
+        @Authorization(value = "bearer")
+    }, tags={ "ExportThemes", "ExportThemeFiles",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "an updated export theme file", response = ExportThemeFile.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Forbidden.class),
+        @ApiResponse(code = 404, message = "Attempted to make a call to an non existant metaform", response = NotFound.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = InternalServerError.class) })
+    public Response updateExportThemeFile(@PathParam("realmId") @ApiParam("realm id") String realmId,@PathParam("exportThemeId") @ApiParam("ExportTheme id") UUID exportThemeId,@PathParam("exportThemeFileId") @ApiParam("ExportThemeFile file id") UUID exportThemeFileId,@Valid ExportThemeFile payload) throws Exception;
 
     @PUT
     @Path("/{realmId}/metaforms/{metaformId}")
